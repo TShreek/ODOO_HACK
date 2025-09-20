0504dcb683ce2cee53af8cfe4ebfdf5f5b178bda
Shreekrishna T <tshreek2004@gmail.com>
2025-09-20 14:08:40 +0530

    implement database file

---
+++ backend/database.py
@@ -0,0 +1,23 @@
+# Database implementation
+from typing import Generator
+from sqlmodel import create_engine, Session, SQLModel
+from shiv_accounts_cloud.config import settings
+from shiv_accounts_cloud.models import auth
+
+
+engine = create_engine(settings.DATABASE_URL, echo=True)
+
+
+def create_db_and_tables():
+    """
+    Creates all tables in the database based on the SQLModel definitions.
+    """
+    SQLModel.metadata.create_all(engine)
+
+
+def get_db() -> Generator[Session, None, None]:
+    """
+    Dependency to get a database session.
+    """
+    with Session(engine) as session:
+        yield session
